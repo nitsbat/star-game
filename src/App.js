97c37a435a6f13b721796e3bc1e1ab3264a855b5
Nitin Bisht <3498georgian@gmail.com>
2022-12-20 17:26:03 +0530

    Added the functions for logic and use color styles.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,104 @@
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+const StarsDisplay = props => (
+  <>
+    {utils.range(1, props.count).map(starId => (
+      <div key={starId} className="star" />
+    ))}
+  </>
+);
+
+const PlayNumber = props => (
+  <button className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => console.log('Num', props.number)}>
+    {props.number}
+  </button>
+);
+
+const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  // candiateNums = Numbers 
+  //availableNums = Numbers which are not yet cicked and are available
+  // usedNums = it is just the opposite of the availableNums
+  // wrongNumber = if selected number + already available candidate numbers is greater than number of stars.
+  const [availableNums, setAvailableNums] = useState([1, 2, 3, 4, 5]);
+  const [candidateNums, setCandidateNums] = useState([2,3]);
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+  const numberStatus = (number) => {
+    if (!availableNums.includes(number))
+      return "used";
+
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? "wrong" : "candidate";
+    }
+
+    return "available";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          <StarsDisplay count={stars} />
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number =>
+            <PlayNumber key={number}
+              number={number}
+              status={numberStatus(number)}
+            />
+          )}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>
   );
-}
+};
 
-export default App;
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
+export default StarMatch;
